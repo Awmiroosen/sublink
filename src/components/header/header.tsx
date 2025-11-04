2b22c7e75a3caf4deab4026873cd5ec908c0f141
Amirx <106537583+Awmiroosen@users.noreply.github.com>
2025-11-04 14:41:54 +0330

    github link added

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import Theme from "./theme";
+import { FiGithub } from "react-icons/fi";
 
 const Header = () => {
   return (
@@ -6,7 +7,14 @@ const Header = () => {
       <div className="w-2/3">
         <h1 className="font-black text-3xl">SubLink</h1>
       </div>
-      <div className="w-1/3 flex justify-end">
+      <div className="w-1/3 grid grid-cols-2 gap-4">
+        <a
+          href="https://github.com/awmiroosen"
+          target="_blank"
+          className="flex items-center justify-center bg-indigo-300 hover:bg-indigo-400 dark:bg-indigo-600 dark:hover:bg-indigo-800 border-2 transition-colors duration-200"
+        >
+          <FiGithub size={25} />
+        </a>
         <Theme />
       </div>
     </header>
