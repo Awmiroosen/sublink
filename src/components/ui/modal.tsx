817d5627cf0a593e9d7e25217d9da1918dd27712
Amirx <106537583+Awmiroosen@users.noreply.github.com>
2025-11-04 14:59:19 +0330

    modal bug fix

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -6,7 +6,7 @@ type ModalType = {
 const Modal = ({ message, visible }: ModalType) => {
   return (
     <div
-      className={`w-xs h-12 bg-indigo-300 dark:bg-indigo-800 border-2 flex justify-center select-none items-center fixed bottom-14 left-[30%] transition-opacity duration-300 
+      className={`w-[23rem] h-12 bg-indigo-300 dark:bg-indigo-800 border-2 flex justify-center select-none items-center fixed bottom-14 left-auto transition-opacity duration-300
         ${visible ? "opacity-100" : "opacity-0 pointer-events-none"}`}
     >
       <p>{message}</p>
